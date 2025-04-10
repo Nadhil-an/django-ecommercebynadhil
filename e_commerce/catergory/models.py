e7130b2f3952a554262b4476b27150afa1f4cb34
Nadhil-an <nadhil.edu@gmail.com>
2025-04-10 23:32:50 +0530

    Added media file and set db browser

--- e_commerce/catergory/models.py
+++ e_commerce/catergory/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name    =  models.CharField(max_length=50, unique=True)
-    slug             =  models.CharField(max_length=100, unique=True)
+    slug             =  models.SlugField(max_length=100, unique=True)
     description      =  models.CharField(max_length=50, blank=True)
     category_img     =  models.ImageField(upload_to='photos/category', blank=True)
     
@@ -13,6 +13,6 @@ class Category(models.Model):
         verbose_name ='category'
         verbose_name_plural = 'categories'
 
-def __str__(self):
-    return self.category_name
+    def __str__(self):
+        return self.category_name
 
