0df2ee56af534b6a9119a81e9be4ac7f5f20bba2
Nadhil-an <nadhil.edu@gmail.com>
2025-04-17 23:03:33 +0530

    solving cart functionality

--- e_commerce/cart/views.py
+++ e_commerce/cart/views.py
@@ -8,26 +8,32 @@ def _cart_id(request):
         cart = request.session.create()
     return cart
 
-def add_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
-    try:
-        cart = Cart.objects.get(cart_id = _cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-    cart.save()
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request,product_id):
+    product = Product.object.get(id=product_id)
 
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart.item.quantity += 1
-        cart_item.save()
+        cart = Cart.objects.get(card_id = _cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id = _cart_id(request))
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity =+ 1
+        cart = cart
+
     except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
-        )
+        cart_item = CartItem.objects.create(product=product,
+                                            quantity = 1,
+                                            cart=cart)
     return redirect('cart')
 
 
--- e_commerce/store/admin.py
+++ e_commerce/store/admin.py
@@ -4,7 +4,7 @@ from .models import Product
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name','price','stock','category','modified','is_available')
+    list_display = ('id','product_name','price','stock','category','modified','is_available')
     prepopulated_fields = {'slug':('product_name',)}
 
 admin.site.register(Product,ProductAdmin)
