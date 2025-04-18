03641b6646cec2ff5bc02a524e3c238e4fbb98ac
Nadhil-an <nadhil.edu@gmail.com>
2025-04-18 22:30:32 +0530

    make modification in add to cart functionallity

--- e_commerce/cart/views.py
+++ e_commerce/cart/views.py
@@ -5,39 +5,30 @@ from store.models import Product
 
 
 def _cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
+    session_key = request.session.session_key
+    if not session_key:
+        session_key = request.session.create()
+    return session_key
 
-
-
-def _cart_id(request):
-    cart = request.session.session_key
-    if not cart:
-        cart = request.session.create()
-    return cart
 
 def add_cart(request,product_id):
-    product = Product.objects.get(id=product_id)
+    my_product = Product.objects.get(id=product_id)
+
+    session_key = _cart_id(request)
 
     try:
-        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart = Cart.objects.get(cart_id = session_key)
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(cart_id = _cart_id(request))
+        cart = Cart.objects.create(cart_id = session_key)
         cart.save()
-
     try:
-        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item = CartItem.objects.get(product=my_product,cart=cart)
         cart_item.quantity += 1
         cart = cart
         cart_item.save()
         
-        
-
-
     except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(product=product,
+        cart_item = CartItem.objects.create(product=my_product,
                                             quantity = 1,
                                             cart=cart)
     
@@ -47,13 +38,15 @@ def add_cart(request,product_id):
 
 # Create your views here.
 def cart(request, total=0, quantity=0, cart_item=None):
+
+    session_key = _cart_id(request)
     
-    cart = Cart.objects.get(cart_id=_cart_id(request))
+    cart = Cart.objects.get(cart_id=session_key)
     cart_items = CartItem.objects.filter(cart=cart, is_active=True)
     for cart_item in cart_items:
         total +=(cart_item.quantity * cart_item.product.price)
         quantity += cart_item.quantity
-    
+
 
     context = {
         'total'     : total,
