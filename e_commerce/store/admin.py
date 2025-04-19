61781240626705eb0df08ca07cf1ffac495c5e76
Nadhil-an <nadhil.edu@gmail.com>
2025-04-20 01:00:23 +0530

    make change in product description

--- e_commerce/store/admin.py
+++ e_commerce/store/admin.py
@@ -5,6 +5,8 @@ from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','product_name','price','stock','category','modified','is_available')
+    list_display_links = ('product_name',)
     prepopulated_fields = {'slug':('product_name',)}
+
 
 admin.site.register(Product,ProductAdmin)
